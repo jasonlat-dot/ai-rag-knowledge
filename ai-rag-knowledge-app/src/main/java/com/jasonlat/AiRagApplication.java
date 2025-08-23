8e865f9ce54b27e498a0fccc61177176b4f64449
jasonlat-dot <lijiaqiangli1024@gmail>
2025-08-23 08:53:27 +0800

    fix: Git仓库代码库解析到知识库 - ui实现优化

--- ai-rag-knowledge-app/src/main/java/com/jasonlat/AiRagApplication.java
+++ ai-rag-knowledge-app/src/main/java/com/jasonlat/AiRagApplication.java
@@ -2,8 +2,10 @@ package com.jasonlat;
 
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +16,14 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEncryptableProperties
 @ComponentScan(basePackages = {"com.jasonlat", "cc.jq1024.middleware"})
 public class AiRagApplication {
+
+    @Value("${app.config.cross-origin}")
+    private String crossOrigin;
+    @PostConstruct
+    public void init() {
+        log.info("Cross Origin: {}", crossOrigin);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AiRagApplication.class);
         log.info("Ai Rag Application Started...");
